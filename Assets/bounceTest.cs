d52b31f6108c7c9403b659350b40c34e19930150
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-04-25 12:52:14 -0700

    Added theater, background crowd noise and one 'person' with test script to move up and down

---
+++ Assets/bounceTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounceTest : MonoBehaviour
+{
+     public float minY, maxY;
+     public bool isMovingUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          minY = transform.position.y;
+          maxY = minY + 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if(transform.position.y < minY)
+          {
+               isMovingUp = true;
+          }
+          else if (transform.position.y > maxY)
+          {
+               isMovingUp = false;
+          }
+
+          if (isMovingUp)
+          {
+               transform.position += transform.up * Time.deltaTime;
+          }
+          else
+          {
+               transform.position -= transform.up * Time.deltaTime;
+          }
+    }
+}
